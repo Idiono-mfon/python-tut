50d089c2f594a5b03ca25bce457ac636abac66e5
Anthony <olympnetwork@gmail.com>
2023-08-12 19:01:02 +0100

    feat: class work on conditional statement

---
+++ conditionalStatement/condition.py
@@ -0,0 +1,52 @@
+
+""""
+
+Comparison Operator
+== 
+>=
+<=
+!=
+<
+>
+"""
+
+print(3 == 3)
+
+print(3 < 3)
+
+print(4 < 3)
+
+print(4 > 20)
+
+print(4 != 20)
+
+
+print(4 <= 3)
+
+print(5 >= 3)
+
+"""
+# Logical Operators
+or
+and
+not
+"""
+
+
+print(4 < 3 or 4 == 3)
+
+print(True or False)
+
+print(True and False)
+
+dom_age = 15
+
+if (dom_age == 14):
+    print(
+        f"My bro Dominion well done you are still a Teenager because your age is {dom_age}")
+else:
+    print("Yoh!, the was false")
+
+print("Holy Molly! I am done executing the conditional statemnt...")
+# if(condition):
+# statement
---
+++ conditionalStatement/elif.py
@@ -0,0 +1,16 @@
+dom_age = 18
+
+if (dom_age == 14):
+    print(
+        f"My bro Dominion well done you are still a Teenager because your age is {dom_age}")
+elif (dom_age == 15):
+    print(
+        f"I am the from elif...Dominison well done you are still a Teenager because your age is {dom_age}")
+
+elif (dom_age == 16):
+    print(
+        f"I am the from elif 2...Dominion well done you are still a Teenager because your age is {dom_age}")
+else:
+    print("Yoh!, the was false")
+
+print("Holy Molly! I am done executing the conditional statemnt...")
